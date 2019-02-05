27a47bd12eb3ff7098abe769ccabd6ad695af390
faheem <faheemkazi@hotmail.com>
2019-02-05 17:04:46 -0600

    assignment 2/4/2019

---
+++ Process.java
@@ -0,0 +1,11 @@
+package firstjavaproject;
+
+public class Process {
+	public int[] xyz(int a, int b) {
+		int[] ans = new int[3];
+		ans[0] = a + b;
+		ans[1] = a - b;
+		ans[2] = a * b;
+		return ans;
+	}
+}
